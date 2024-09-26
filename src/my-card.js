c0e6f93f2ac27f85af9236b25394cd30036dbc4b
SkylerKoba88 <sfk5879@psu.edu>
2024-09-26 15:16:53 -0400

    message goes here

--- src/my-card.js
+++ src/my-card.js
@@ -8,48 +8,63 @@ import { LitElement, html, css } from 'lit';
 
 export class MyCard extends LitElement {
 
+  openChanged(e) {
+    console.log(e.newState);
+    if (e.newState === "open") {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
+  }
+
   static get tag() {
     return 'my-card';
   }
 
   constructor() {
     super();
-    this.title = "title-default";
-    this.bTitle = "card-default";
+    this.title = "";
+    this.buttonTitle = "";
     this.link = "#";
-    this.image = '#';
-    this.text = 'text-default';
-    this.text2 = "text-default";
+    this.image = null;
+    this.line = "";
+    this.fancy = false;
   }
 
   static get styles() {
     return css`
       :host {
-        display: block;
+        display: inline-flex;
         font-family: Verdana, Geneva, Tahoma, sans-serif;
+        background-color: white;
+        margin: 24px;
+        max-width: 390px;
+        border-radius: 16px;
+        border: solid black 8px;
+      }
+      :host([fancy]) {
+        display: inline-flex;
+        background-color: #eb9eb9;
+        border: outset #a11c53 8px;
       }
       div {
-        background-color: #FFFFFF;
         text-decoration-style:none;
-        border: solid black 8px;
-        margin: 64px;
         padding: 16px;
-        max-width: 400px;
         width: auto;
-        border-radius: 16px;
       }
       a {
         border-radius: 8px;
         font-size: 16px;
         border-style: none;
-        background-color: #be2528;
+        background-color: var(--my-card-change-bg, #be2528);
         color: white;
         padding: 8px;
         font-weight: bold;
         text-decoration: none;
       }
       a:hover {
-        background-color: #f0e68c;
+        background-color: var(--my-card-change-hover, #f0e68c);
         color: white;
       }
       img {
@@ -58,21 +73,39 @@ export class MyCard extends LitElement {
         width: 100%;
         margin: auto;
       }
+      details {
+        padding-bottom: 16px;
+        font-style: italic;
+      }
     `;
   }
 
   render() {
-    return html`<div><img src='${this.image}'><h3>${this.title}</h3><p>${this.text}</p><p>${this.text2}</p><a href='${this.link}'>${this.bTitle}</div>`;
+    return html`
+    <div><img src='${this.image}'>
+    <h3>${this.title}</h3>
+    <p>${this.line}<strong><slot name="PBP"></slot></strong></p>
+    <p><slot name="CCC"></slot></p>
+    <p><slot name="KY"></slot></p>
+    <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+      <summary>Description</summary>
+      <p><slot name="detes"></slot></p>
+    </details>
+    <a href='${this.link}' target="_blank">${this.buttonTitle}</div>`;
   }
+    /*const dropdown = ('detes')? <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+      <summary>Description</summary>
+      <p><slot name="detes"></slot></p>
+    </details>;*/
 
   static get properties() {
     return {
       title: { type: String},
-      bTitle: { type: String },
+      buttonTitle: { type: String },
       link: { type: String},
       image: { type: String},
-      text: { type: String},
-      text2: { type: String}
+      line: { type: String},
+      fancy: {type: Boolean, reflect: true}
     };
   }
 }
